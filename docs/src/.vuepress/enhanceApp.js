c46bb9a65e154bb12ccb9cdc409e7ab4721f9502
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-12-06 16:09:57 +0700

    Add Login

--- docs/src/.vuepress/enhanceApp.js
+++ docs/src/.vuepress/enhanceApp.js
@@ -1,14 +1,42 @@
-/**
- * Client app enhancement file.
- *
- * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
- */
+import { checkAuth } from './login/helper'
+import Login from './login/Login'
 
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
+  Vue,
+  options,
+  router,
+  siteData
 }) => {
-  // ...apply enhancements for the site.
+  Vue.mixin({
+    mounted() {
+      const doCheck = () => {
+        if (!checkAuth()) {
+          this.$dlg.modal(Login, {
+            width: 300,
+            height: 350,
+            title: 'Login Documentations',
+            singletonKey: 'employee-login',
+            maxButton: false,
+            closeButton: false,
+            callback: data => {
+              if (data === true) {
+                // do some stuff after login
+              }
+            }
+          })
+        }
+      }
+
+      if (this.$dlg) {
+        doCheck()
+      } else {
+        import('v-dialogs').then(resp => {
+          Vue.use(resp.default)
+          this.$nextTick(() => {
+            doCheck()
+          })
+        })
+      }
+    }
+  })
 }
